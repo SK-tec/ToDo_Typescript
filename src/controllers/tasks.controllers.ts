043aa2a0d4ac954afe25a07ceae803287f3c7308
samatha <samathagamidi@gmail.com>
2023-09-09 11:52:48 +0200

    Code to Update and Delete Task added

--- src/controllers/tasks.controllers.ts
+++ src/controllers/tasks.controllers.ts
@@ -7,23 +7,68 @@ export const createTask = async (req: Request, res: Response) => {
     //console.log(newTask);
     res.status(201).json({ message: "Task created successfully" });
   } catch (error) {
-    if (error instanceof Error) {
-      res.status(500).json({ message: error.message });
-    } else {
-      res.status(500).json({ message: "Unkown error" });
-    }
+    res.status(500).json({
+      message: error instanceof Error ? error.message : "Unknown type",
+    });
   }
 };
 
 export const getAllTasks = async (req: Request, res: Response) => {
   try {
     const tasks = await Task.find();
+    if (tasks.length === 0) {
+      res.status(404).json({ message: "Tasks not found" });
+    }
     res.json(tasks);
   } catch (error) {
     if (error instanceof Error) {
       res.status(500).json({ message: error.message });
     } else {
-      res.status(500).json({ message: "Unkown error" });
+      res.status(500).json({ message: "Unknown error" });
     }
+  }
+};
+export const getTaskById = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const task = await Task.findById({ _id: id });
+    if (!task) {
+      res.status(404).json({ message: "Task not found" });
+    }
+    res.json(task);
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: e instanceof Error ? e.message : "Unknown type error" });
+  }
+};
+export const updateTask = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const task = await Task.findOneAndUpdate({ _id: id }, req.body, {
+      new: true,
+    });
+    console.log("update task executed");
+    res.json(task);
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: e instanceof Error ? e.message : "Error is Unknown" });
+  }
+};
+
+export const deleteTask = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const task = await Task.findByIdAndDelete({ _id: id });
+    if (!task) {
+      res.status(404).json({ message: "Task not found" });
+    }
+
+    res.json(task);
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: e instanceof Error ? e.message : "Unknown Error" });
   }
 };
--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,7 +1,16 @@
 import { Router } from "express";
-import { createTask, getAllTasks } from "../controllers/tasks.controllers";
+import {
+  createTask,
+  getAllTasks,
+  getTaskById,
+  updateTask,
+  deleteTask,
+} from "../controllers/tasks.controllers";
 
 export const router = Router();
 
 router.get("/", getAllTasks);
+router.get("/:id", getTaskById);
 router.post("/", createTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
